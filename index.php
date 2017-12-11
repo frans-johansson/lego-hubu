bcdd14c053cf20226bc202f2b15c950c511a650a
frans-johansson <fransjohansson98@gmail.com>
2017-12-11 13:42:42 +0100

    Implementerar GET-variablar för att hantera navigation i index-fil

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-    include "pages/home.php";
+    if (!isset($GET['page']))
+    {
+        include "pages/home.php";
+    }
+    else if ($GET['page'] == "home")
+    {
+        include "pages/home.php";
+    }
+    else if ($GET['page'] == "sets")
+    {
+        include "pages/sets.php";
+    }
+    else if ($GET['page'] == "parts")
+    {
+        include "pages/parts.php";
+    }
 ?>
