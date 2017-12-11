f26a0ef031436c9426e8bc28da80a6ef51b325e8
frans-johansson <fransjohansson98@gmail.com>
2017-12-11 09:39:47 +0100

    Ytterligare segmentering av filer med php-includes

---
+++ pages/home/homePage.php
@@ -0,0 +1,10 @@
+<div id="page">
+        <!--Inkludera fil med övre sidinnehållet-->
+        <?php
+            include "upperHome.html"
+        ?>
+        <!--Inkludera diagram-fil-->
+        <?php
+            include "diagram.html"
+        ?>
+</div>
