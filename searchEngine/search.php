8c42247de366406125bf74b0579c72b71808569a
Matilda Eriksson <mater436@student.liu.se>
2017-12-13 16:47:25 +0100

    Fixat formuleringen på frågorna och fixat filtreringen, fortfarande arbete kvar på att göra det möjligt att söka på flera saker samtidigt

--- searchEngine/search.php
+++ searchEngine/search.php
@@ -10,49 +10,75 @@ if(!$lowerLimit || $lowerLimit < 0) {
 
 $displaylimit = 20;
 
-if($_GET["condition"] == "sets"){
+// Få fram vilka frågor som ska ställas
+if($_GET["set"] != ""){
+	$set = $_GET["set"];
 	$table = "";
-	$where1 = "SetID";				// Kom på bättre variabelnamn!
+	$where1 = "inventory.SetID LIKE '%" . $set . "%'";				// Kom på bättre variabelnamn!
 	$where2 = "Setname";
-	$or = "OR " . $where2 . " LIKE '%" . $search . "%'";
+	$or = " OR " . $where2 . " LIKE '%" . $set . "%'";
 }
-else if($_GET["condition"] == "parts"){
+else if($_GET["par"] != ""){
+	$par = $_GET["par"];
 	$table = "";
-	$where1 = "PartID";	
+	$where1 = "PartID LIKE '%" . $par . "%'";
 	$where2 = "Partname";
-	$or = "OR " . $where2 . " LIKE '%" . $search . "%'";
+	$or = " OR " . $where2 . " LIKE '%" . $par . "%'";
 }
-else if($_GET["condition"] == "color"){
-	$table = ", colors";
-	$where1 = "Colorname";	
-	$or = "";
-}
-else if($_GET["condition"] == "year"){
+else if($_GET["col"] != ""){
+	$col = $_GET["col"];
 	$table = "";
-	$where1 = "Year";	
+	$where1 = "Colorname LIKE '%" . $col . "%'";	
 	$or = "";
 }
-else if($_GET["condition"] == "category"){
-	$table = ", categories";
-	$where1 = "Categoryname";	
+else if($_GET["yea"] != ""){
+	$yea = $_GET["yea"];
+	$table = "";
+	$where1 = "Year LIKE '%" . $yea . "%'";	
 	$or = "";
-}	
+}
+else if($_GET["cat"] != ""){
+	$cat = $_GET["cat"];
+	$table = ", categories";
+	$where1 = "Categoryname LIKE '%" . $cat . "%' AND parts.CatID = categories.CatID AND categories.CatID = sets.SetID";	
+	$or = "";
+}
+
+
+// Få fram i vilken ordning obejekten ska visas
+if($_GET["f"] == "ageAsc") {
+	$order = "MIN(Year) ASC";
+}
+else if($_GET["f"] == "ageDesc") {
+	$order = "MIN(Year) DESC";
+}
+else if($_GET["f"] == "rarityAsc") {
+	$order = "COUNT(DISTINCT inventory.SetID) ASC";
+}
+else if($_GET["f"] == "rarityDesc") {
+	$order = "COUNT(DISTINCT inventory.SetID) DESC";
+}
+
+$group = "Colorname";
+// Eller $group = vad?
+
 
 //Sök
 if (!empty($_GET["search"])) {
 		$search = $_GET["search"];
 	}
-	$searchQuery = "SELECT	PartID, Partname, Colorname FROM parts, inventory, sets, colors" . $table . " WHERE PartID = ItemID AND inventory.ColorID = colors.ColorID AND " . $where1 . " LIKE '%" . $search . "%' 
-	" . $or . " LIMIT " . $lowerLimit * $displaylimit . " ," . $displaylimit;
 	
-	// parts, inventory, sets, colors, categories
+// Skapa sökfrågan
+$searchQuery = "SELECT	PartID, Partname, Colorname, COUNT(DISTINCT inventory.SetID), MIN(Year) FROM parts, inventory, sets, colors" . $table . " WHERE PartID = ItemID AND inventory.ColorID = colors.ColorID AND 
+				ItemTypeID = 'P' AND inventory.SetID = sets.SetID AND" . $where1 . $or . " GROUP BY " . $group . " ORDER BY " . $order . " LIMIT " . $lowerLimit * $displaylimit . " ," . $displaylimit;
+
 	
-	
+// Testa om det går bra att koppla upp mot databasen
 $connection = mysqli_connect("mysql.itn.liu.se","lego","","lego");
 	
 	if (!$link) {
 		echo "Error: Unable to connect to MySQL." . PHP_EOL;
-	echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+		echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
 		echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
 		exit;
 	}
@@ -61,24 +87,23 @@ $connection = mysqli_connect("mysql.itn.liu.se","lego","","lego");
 	$result	= mysqli_query($connection, "$searchQuery");
 	
 while($row = mysqli_fetch_array($result)) {
+	// Lägg informationen som ska visas i separata variabler
 	$ID = $row["PartID"];
 	$Partname = $row["Partname"];
 	$Color = $row["Colorname"];
-	
-	$numSets = mysqli_query($connection, "SELECT COUNT(DISTINCT SetID) FROM inventory, parts 
-							WHERE ItemTypeID = 'P' AND PartID = '$ID' AND PartID = ItemID");
-	
-	$Year = mysqli_query($connection, "SELECT DISTINCT Year FROM sets, inventory WHERE ItemID = '$ID' 
-										AND sets.SetID = inventory.SetID ORDER BY Year ASC LIMIT 1");
-								
+	$numSets = $row["COUNT(DISTINCT inventory.SetID)"];
+	$Year = $row["MIN(Year)"];
+
+		
+	// Fråga efter den information som är relevant för att få fram en bild
 	$format = mysqli_query($connection, "SELECT colors.ColorID, ItemTypeID, ItemID, has_gif, has_jpg, has_largegif, has_largejpg FROM images, colors 
 										WHERE ItemID = '$ID' AND Colorname = '$Color' AND colors.ColorID = images.ColorID");
-								
+	// Lägg den nödvändiga informationen för bildnamnet i variabler
 	$Itemtype = $format["ItemTypeID"];
 	$ColorID = $format["ColorID"];
 	$ItemID = $format["ItemID"];
 								
-	// Bild
+	// Bilda länken till den bild som ska visas
 	if($format["has_jpg"]) {
 		$name = $Itemtype . '/' . $ColorID . '/' . $ItemID . '.jpg';
 		$link = 'http://www.itn.liu.se/~stegu76/img.bricklink.com/$name';
@@ -102,7 +127,7 @@ while($row = mysqli_fetch_array($result)) {
 	" . $or . " GROUP BY ItemID ORDER BY COUNT(DISTINCT SetID) LIMIT " . $lowerLimit * $displaylimit . " ," . $displaylimit;
 
 	
-	SELECT	PartID, Partname, Colorname, COUNT(DISTINCT inventory.SetID) FROM parts, inventory, colors WHERE PartID = ItemID AND inventory.ColorID = colors.ColorID 
-	AND ItemTypeID = 'P' AND PartID LIKE '%320%' GROUP BY ItemID ORDER BY COUNT(DISTINCT inventory.SetID) DESC LIMIT 20
+	SELECT PartID, Partname, Colorname, COUNT(DISTINCT inventory.SetID), MIN(Year) FROM parts, inventory, colors, sets WHERE PartID = '3003' AND PartID = ItemID 
+	AND inventory.ColorID = colors.ColorID AND ItemTypeID = 'P' AND inventory.SetID = sets.SetID GROUP BY Colorname ORDER BY MIN(Year) ASC LIMIT 20
 */
 ?>
