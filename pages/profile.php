bdc0098a47713935b60efb3a4a93f8ae25296277
Frans Johansson <frajo752@student.liu.se>
2017-12-15 16:39:33 +0100

    Arbete med pop-up åt profilen. Just nu är den lite på fel sida av skärmen men det fixar vi sen :)

--- pages/profile.php
+++ pages/profile.php
@@ -1,8 +1,12 @@
 <div id="profilePopup" onclick="togglePopup()">
- <div id="profileSets">
-    <p>Sets</p>
- </div>
- <div id="profileParts">
-    <p>Parts</p>
- </div>
+	<h1>Your collection</h1>
+	<div id="profileWrapper">
+		<div id="profileSets">
+			<p>Sets</p>
+		</div>
+		<div id="profileParts">
+			<p>Parts</p>
+		</div>
+	</div>
 </div>
+
--- scripts/profile.js
+++ scripts/profile.js
@@ -4,12 +4,12 @@ function togglePopup(){
 	
 	if(hidden) {
 		document.getElementById("profile").style.display = "none";
-		document.getElementById("profilePopup").style.display = "inline-block";
+		document.getElementById("profilePopup").style.display = "block";
 		hidden = !hidden;
 	}
 	else {
 		document.getElementById("profilePopup").style.display = "none";
-		document.getElementById("profile").style.display = "inline-block";
+		document.getElementById("profile").style.display = "block";
 		hidden = !hidden;
 	}
 }
