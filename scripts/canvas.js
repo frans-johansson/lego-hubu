c3ee8060a218c4a47c536f2c9555e220e2446ba9
Frans Johansson <frajo752@student.liu.se>
2017-12-19 12:16:19 +0100

    JS för tolkning av tabelldata samt diagram i canvas

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,32 +1,83 @@
+var c;
+var data = new Array();
+var maxAmount;
 
-window.onload = function(){
-	
-	var c=document.getElementById("canvas");
-	var ctx=c.getContext("2d");
-	
-	/* Bestämma bredd och höjd */
-	
-	var parentWidth = c.parentNode.getBoundingClientRect().width;
-	
-	c.width = parentWidth;
-	c.height = 400px;
+function DataEntry (year, amount) {
+	this.year = year;
+	this.amount = amount;
+}
 
-	ctx.beginPath();
-	ctx.moveTo(0,0);
-	ctx.lineTo(100,150);
-	ctx.lineTo(200,200);
-	ctx.lineTo(300,100);
-	ctx.lineTo(400,150);
-	ctx.lineTo(500,10);
-	ctx.lineTo(600,100);
-	ctx.lineTo(700,300);
-	ctx.lineTo(800,200);
-	ctx.stroke();
+initCanvas = function() {
+	c = document.getElementById("canvas");
 	
-	ctx.beginPath();
-    ctx.arc(100,150,10,0,2*Math.PI);
-    ctx.stroke();
 	
+	setArray();
+	getMax();
+	stageCanvas();
+}
+
+stageCanvas = function() {
+	var ratio = 16/9;
+	
+	var width = c.parentNode.clientWidth;
+	var height = width/ratio;
+	
+	c.width = width;
+	c.height = height;
+	
+	drawCanvas();
+}
+
+drawCanvas = function() {
+	var ctx = c.getContext("2d");
+	
+	var unitY = c.height/maxAmount;
+	var unitX = c.width/data.length;
+	
+	ctx.fillStyle = "#fff";
+	
+	for (var i = 0; i < data.length; i++) {
+		ctx.fillRect(unitX + i * unitX, c.height, -unitX, -data[i].amount * unitY);
+		console.log(data[i].year);
+	}
+}
+
+setArray = function() {
+	var resultTable = document.getElementById("diagramData").children[0];
+	var resultRows = resultTable.children;
+	
+	var firstYear = resultRows[0].children[0].innerHTML;
+	var lastYear = resultRows[resultRows.length - 2].children[0].innerHTML; //pga frågetecken (alla utan år får ? och läggs i sista raden, därav -3)
+
+	
+	var currentYear = firstYear;
+
+	for(var i = 0; i < lastYear - firstYear - 1; i++){
+		var year = resultRows[i].children[0].innerHTML;
+		var amount = resultRows[i].children[1].innerHTML;
+		
+		while (year > currentYear) {
+			data.push(new DataEntry(currentYear, 0));
+			currentYear++;
+		}
+
+		data.push(new DataEntry(year, amount));
+		
+		currentYear++;
+	}
+}
+
+getMax = function() {
+	maxAmount = 0;
+	
+	for (var i = 0; i < data.length; i++) {
+		if (parseInt(data[i].amount) > maxAmount) {
+			maxAmount = data[i].amount;
+			console.log(maxAmount);
+		}
+	}
 }
 
 
+window.addEventListener("load", initCanvas);
+window.addEventListener("resize", stageCanvas);
