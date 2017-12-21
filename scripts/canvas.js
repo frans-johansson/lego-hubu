13db4ef6504ae87eb2bd4e697a8c5f5daaa5199c
Frans Johansson <frajo752@student.liu.se>
2017-12-21 12:46:13 +0100

    Gör canvas elementet smartare. Organiserar staplarna i en array med information om år och antal. Implementerar även en slags onhover funktion som ändrar färg på staplar samt skriver ut informationen i övre hörnet. Måste fixa med färger dock!

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,11 +1,23 @@
 var c;
+var ctx;
 var width, height;
-var data = new Array();
+var data = new Array(); // En array med DataEntry
 var maxAmount;
+var rects = new Array(); // En array med Rect
 
 function DataEntry (year, amount) {
 	this.year = year;
 	this.amount = amount;
+}
+
+function Rect (x, y, w, h, data) {
+	this.x = x;
+	this.y = y;
+	this.w = w;
+	this.h = h;
+	this.data = data; 		// Detta ska vara det DataEntry som motsvarar just den stapeln
+	
+	var hasHover = false; 	// För att avgöra om en stapel har musen över sig
 }
 
 initCanvas = function() {
@@ -15,6 +27,8 @@ initCanvas = function() {
 	getMax();
 	stageCanvas();
 	drawCanvas();
+	
+	c.addEventListener("mousemove", checkHover);
 }
 
 stageCanvas = function() {
@@ -25,19 +39,82 @@ stageCanvas = function() {
 	
 	c.width = width;
 	c.height = height;
+	
+	addRects(); //Lägger till alla staplar efter alla höjd- och breddparametrar har beräknats (då även onresize)
 }
 
-drawCanvas = function() {
-	var ctx = c.getContext("2d");
-	
+addRects = function() {
 	var unitY = height/maxAmount;
 	var unitX = width/data.length;
 	
-	ctx.fillStyle = "#fff";
-	
 	for (var i = 0; i < data.length; i++) {
-		ctx.fillRect(unitX + i * unitX, height, -unitX, -data[i].amount * unitY);
+		x = unitX + i * unitX
+		y = height;
+		w = -unitX;
+		h = -data[i].amount * unitY;
+		
+		rects[i] = new Rect(x, y, w, h, data[i]); // Lägger till stapeln som nu ska ritas i arrayen med alla staplar
 	}
+}
+
+drawCanvas = function() {
+	ctx = c.getContext("2d");
+	ctx.font = "30px Arial";
+	
+	var rectInfo = "";
+	
+	for (var i = 0; i < rects.length; i++) {
+		ctx.beginPath();
+		
+		// Se om "onhover"-information borde visas
+		if (rects[i].hasHover) {
+			
+			rectInfo = 	"Year: " + rects[i].data.year +
+						"\nAmount: " + rects[i].data.amount;
+						
+			var textWidth = ctx.measureText(rectInfo);
+			
+			ctx.rect(10, 30, textWidth, 30);
+			ctx.fillStyle = "#3872A9";
+			ctx.fill();
+			
+			ctx.fillStyle = "#F0C247";
+		}
+		else {
+			ctx.fillStyle = "#fff";
+			rectInfo = "";
+		}
+		
+		ctx.rect(rects[i].x, rects[i].y, rects[i].w, rects[i].h);
+		ctx.fill();
+		
+		ctx.fillText(rectInfo, 10, 30);
+	}
+}
+
+checkHover = function(pointer) {
+	// Sätter x och y till muspekarens koordinater i canvas-elementet (inte på hela sidan)
+	var bounds = c.getBoundingClientRect(),
+		x = pointer.clientX - bounds.left,
+		y = pointer.clientY - bounds.top;
+	
+	
+	for (var i = 0; i < rects.length; i++) {
+		ctx.beginPath();
+		ctx.rect(rects[i].x, rects[i].y, rects[i].w, -height); //Gör en spökrektangel för varje stapel
+		
+		if (ctx.isPointInPath(x, y)) { // Se om muspekaren ligger i denna spökrektangel
+			rects[i].hasHover = true;
+		}
+		else {
+			rects[i].hasHover = false;
+		}
+		
+		ctx.closePath();
+	}
+	
+	ctx.clearRect(0, 0, width, height);
+	drawCanvas();
 }
 
 setArray = function() {
