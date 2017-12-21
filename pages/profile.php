1177755dc0b9cb659de0bcef2ce83eab8389769a
Frans Johansson <frajo752@student.liu.se>
2017-12-21 15:24:14 +0100

    Gör användarens pop-up centrerad på skärmen och med en mörkt tintad bakgrund för resten av hemsidan

--- pages/profile.php
+++ pages/profile.php
@@ -1,17 +1,19 @@
-<div id="profilePopup" onclick="togglePopup()">
-	<h1>Your collection</h1>
-	<div id="profileWrapper">
-		<div id="profileSets">
-			<p>Sets</p>
-			<?php
-				include "profile/setsCollect.php";
-			?>
-		</div>
-		<div id="profileParts">
-			<p>Parts</p>
-			<?php
-				include "profile/partsCollect.php";
-			?>
+<div id="pageBackground">
+	<div id="profilePopup" onclick="togglePopup()">
+		<h1>Your collection</h1>
+		<div id="profileWrapper">
+			<div id="profileSets">
+				<p>Sets</p>
+				<?php
+					include "profile/setsCollect.php";
+				?>
+			</div>
+			<div id="profileParts">
+				<p>Parts</p>
+				<?php
+					include "profile/partsCollect.php";
+				?>
+			</div>
 		</div>
 	</div>
 </div>
