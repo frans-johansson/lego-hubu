2571c36330b04ab1d048d5a960375d6f1bdeb9c2
Frans Johansson <frajo752@student.liu.se>
2018-01-12 15:43:04 +0100

    Gör så att användaren kan klicka var som helst i fönstret för att stänga popup-fönstret för profilen

--- scripts/profile.js
+++ scripts/profile.js
@@ -8,12 +8,20 @@ togglePopup = function(){
 		document.getElementById("profile").style.display = "none";
 		document.getElementById("profilePopup").style.display = "block";
 		document.getElementById("pageBackground").style.display = "flex";
+		
+		// Så att användaren kan klicka var som helst på sidan för att stänga popup-fönstret
+		document.getElementById("pageBackground").addEventListener("click", togglePopup);
+		
 		hidden = !hidden;
 	}
 	else {
 		document.getElementById("profilePopup").style.display = "none";
 		document.getElementById("pageBackground").style.display = "none";
 		document.getElementById("profile").style.display = "block";
+		
+		// Så att inte popup-fönstret öppnas av att användaren klickar var som helst på sidan när inte popup-fönstret visas
+		document.getElementById("pageBackground").removeEventListener("click", togglePopup);
+		
 		hidden = !hidden;
 	}
 }
