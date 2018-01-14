c128360cfdf5ff6a2b23c62ba3edee75628265a2
Matilda Eriksson <mater436@student.liu.se>
2018-01-14 19:11:11 +0100

    tagit bort topplistan över bitar och lagt till kommentarer

--- pages/home/toplist.php
+++ pages/home/toplist.php
@@ -1,22 +1,25 @@
 <?php
 
-
-
 // Koppla upp mot databasen
     include "searchEngine/connect.php";
 
-// Skapa frågan till topplistan över antalet satser med flest bitar 
+// Skapa frågan till topplistan över antalet satser med flest bitar
+// Vi valde LIMIT 10 så att vi får en topp tio-lista
 	$searchQuery = "SELECT Setname, SUM(inventory.Quantity) FROM sets, inventory 
 					WHERE ItemTypeID = 'P' AND sets.SetID = inventory.SetID GROUP BY sets.SetID
 					ORDER BY SUM(inventory.Quantity) DESC LIMIT 10";
 
+/* Påbörja tabellen och skriv ut tabellhuvudena
+Tabellen ligger i en div och första tabellhuvudet spänner över två kolonner,
+en för vilket nummer satsen ligger på och en för namnet*/
 	print "<div class='toplist' id='setsList'>
 			<table>
 				<tr>
 					<th colspan=2>Setname</th>
 					<th>Number of parts</th>
 				</tr>";
-				
+		
+// Denna variabel används för att skriva ut vilken placering i topplistan satsen har		
 	$toplistRow = 1;
 					
 // Ställ frågan till databasen
@@ -29,52 +32,23 @@
 			$name = $row["Setname"];
 			$quantity = $row["SUM(inventory.Quantity)"];
 			
+		// Skriv ut placeringsnumret, namnet och antalet bitar satsen består av
 			print 	"<tr>
 						<td>$toplistRow</td>
 						<td>$name</td>
 						<td>$quantity</td>
 					<tr>";
 					
+		// Öka variabeln med ett så att nästa sats visas med det efterföljande placeringsnumret
 			$toplistRow += 1;
 	}
 	
-	print "</div>";
+// Avsluta tabellen och diven
+	print "</table></div>";
 
-	// Skapa frågan till databasen	
-		$searchQuery = "SELECT Partname, COUNT(DISTINCT inventory.SetID)
-							FROM parts, inventory, sets WHERE PartID = ItemID AND
-							ItemTypeID = 'P' AND inventory.SetID = sets.SetID 
-							GROUP BY PartID ORDER BY COUNT(DISTINCT inventory.SetID) DESC LIMIT 10";
-							
-		print "<div class='toplist' id='partsList'>
-			<table>
-				<tr>
-					<th colspan=2>Partname</th>
-					<th>Included in sets</th>
-				</tr>";
-				
-	$toplistRow = 1;
-					
-// Ställ frågan till databasen
-	$result	= mysqli_query($connection, "$searchQuery");
 	
-// Medan det finns ett resultat, skriv ut detta
-	while($row = mysqli_fetch_array($result)) {
-		// Lägg informationen som ska visas i separata variabler
-			$name = $row["Partname"];
-			$quantity = $row["COUNT(DISTINCT inventory.SetID)"];
-			
-			print 	"<tr>
-						<td>$toplistRow</td>
-						<td>$name</td>
-						<td>$quantity</td>
-					<tr>";
-					
-			$toplistRow += 1;
-	}
-								
-	// Stänger kopplingen till databasen
-        mysqli_close($connection);
+// Stäng kopplingen till databasen
+    mysqli_close($connection);
 
 ?>
 
